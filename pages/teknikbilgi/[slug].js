833445cb572190eda0872126094d346c7a2ac54b
unknown <cagakdemir@hotmail.com>
2021-06-03 11:31:59 +0300

    corrected 10

--- pages/teknikbilgi/[slug].js
+++ pages/teknikbilgi/[slug].js
@@ -3,8 +3,78 @@ import ReactHtmlParser from 'react-html-parser';
 import Layout from '../../components/Layout';
 import { API_URL } from '../../config/index';
 
-export default function BlogPost({ post, styledContent }) {
-  console.log(styledContent)
+const replace = require('str-replace');
+
+export default function BlogPost({ post }) {
+  let styledContent = post.content;
+
+  styledContent = replace
+    .all('<p>')
+    .from(styledContent)
+    .with('<p style="margin-bottom:0.75rem; margin-top: 0.75rem;">');
+  styledContent = replace
+    .all('<p style="text-align:justify;">')
+    .from(styledContent)
+    .with(
+      '<p style="text-align:justify; margin-bottom:0.75rem; margin-top: 0.75rem;">'
+    );
+  styledContent = replace
+    .all('<p style="text-align:center;">')
+    .from(styledContent)
+    .with(
+      '<p style="text-align:center; margin-bottom:0.75rem; margin-top: 0.75rem;">'
+    );
+  styledContent = replace
+    .all('<p style="text-align:right;">')
+    .from(styledContent)
+    .with(
+      '<p style="text-align:right; margin-bottom:0.75rem; margin-top: 0.75rem;">'
+    );
+  styledContent = replace
+    .all('<h2>')
+    .from(styledContent)
+    .with(
+      '<h2 style="text-align:justify; font-size:1.25rem; font-weight: 600;">'
+    );
+  styledContent = replace
+    .all('<h2 style="text-align:justify;">')
+    .from(styledContent)
+    .with(
+      '<h2 style="text-align:justify; font-size:1.25rem; font-weight: 600;">'
+    );
+  styledContent = replace
+    .all('<h2 style="text-align:center;">')
+    .from(styledContent)
+    .with(
+      '<h2 style="text-align:center; font-size:1.25rem; font-weight: 600;">'
+    );
+  styledContent = replace
+    .all('<h2 style="text-align:right;">')
+    .from(styledContent)
+    .with(
+      '<h2 style="text-align:right; font-size:1.25rem; font-weight: 600;">'
+    );
+  styledContent = replace
+    .all('<h3>')
+    .from(styledContent)
+    .with('<h3 style="font-size:1rem; font-weight: 600;">');
+  styledContent = replace
+    .all('<h3 style="text-align:justify;">')
+    .from(styledContent)
+    .with('<h3 style="text-align:justify; font-size:1rem; font-weight: 600;">');
+  styledContent = replace
+    .all('<h3 style="text-align:center;">')
+    .from(styledContent)
+    .with('<h3 style="text-align:center; font-size:1rem; font-weight: 600;">');
+  styledContent = replace
+    .all('<h3 style="text-align:right;">')
+    .from(styledContent)
+    .with('<h3 style="text-align:right; font-size:1rem; font-weight: 600;">');
+  styledContent = replace
+    .all('<ul>')
+    .from(styledContent)
+    .with('<ul style="list-style: inside;">');
+
   return (
     <Layout
       title={post.title}
@@ -25,79 +95,9 @@ export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/teknikbilgis/${slug}`);
   const post = await res.json();
 
-  let styledContent = ''
-
-  // <p> styling
-  styledContent = post.content.replace(
-    '<p>',
-    '<p style="margin-bottom:0.75rem; margin-top: 0.75rem;">'
-  );
-  // styledContent = styledContent.replaceAll(
-  //   '<p style="text-align:justify;">',
-  //   '<p style="text-align:justify; margin-bottom:0.75rem; margin-top: 0.75rem;">'
-  // );
-  // styledContent = styledContent.replaceAll(
-  //   '<p style="text-align:center;">',
-  //   '<p style="text-align:center; margin-bottom:0.75rem; margin-top: 0.75rem;">'
-  // );
-  // styledContent = styledContent.replaceAll(
-  //   '<p style="text-align:right;">',
-  //   '<p style="text-align:right; margin-bottom:0.75rem; margin-top: 0.75rem;">'
-  // );
-
-  // // <h2> styling
-  // styledContent = styledContent.replaceAll(
-  //   '<h2>',
-  //   '<h2 style="font-size:1.25rem; font-weight: 600;">'
-  // );
-
-  // styledContent = styledContent.replaceAll(
-  //   '<h2 style="text-align:justify;">',
-  //   '<h2 style="text-align:justify; font-size:1.25rem; font-weight: 600;">'
-  // );
-
-  // styledContent = styledContent.replaceAll(
-  //   '<h2 style="text-align:center;">',
-  //   '<h2 style="text-align:center; font-size:1.25rem; font-weight: 600;">'
-  // );
-
-  // styledContent = styledContent.replaceAll(
-  //   '<h2 style="text-align:right;">',
-  //   '<h2 style="text-align:right; font-size:1.25rem; font-weight: 600;">'
-  // );
-
-  // // <h3> styling
-  // styledContent = styledContent.replaceAll(
-  //   '<h3>',
-  //   '<h3 style="font-size:1rem; font-weight: 600;">'
-  // );
-
-  // styledContent = styledContent.replaceAll(
-  //   '<h3 style="text-align:justify;">',
-  //   '<h3 style="text-align:justify; font-size:1rem; font-weight: 600;">'
-  // );
-
-  // styledContent = styledContent.replaceAll(
-  //   '<h3 style="text-align:center;">',
-  //   '<h3 style="text-align:center; font-size:1rem; font-weight: 600;">'
-  // );
-
-  // styledContent = styledContent.replaceAll(
-  //   '<h3 style="text-align:right;">',
-  //   '<h3 style="text-align:right; font-size:1rem; font-weight: 600;">'
-  // );
-
-  // // <ul> styling
-  // styledContent = styledContent?.replaceAll(
-  //   '<ul>',
-  //   '<ul style="list-style: inside;">'
-  // );
-
-
   return {
     props: {
       post: post,
-      styledContent: styledContent,
     },
   };
 }
