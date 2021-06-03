2025a64128902f80736f36d9737f880db93c184f
cagakdemir <62038023+cagakdemir@users.noreply.github.com>
2021-06-03 08:18:11 +0300

    8

--- pages/teknikbilgi/[slug].js
+++ pages/teknikbilgi/[slug].js
@@ -4,6 +4,7 @@ import Layout from '../../components/Layout';
 import { API_URL } from '../../config/index';
 
 export default function BlogPost({ post, styledContent }) {
+  console.log(styledContent)
   return (
     <Layout
       title={post.title}
@@ -31,66 +32,66 @@ export async function getServerSideProps({ query: { slug } }) {
     '<p>',
     '<p style="margin-bottom:0.75rem; margin-top: 0.75rem;">'
   );
-  styledContent = styledContent.replaceAll(
-    '<p style="text-align:justify;">',
-    '<p style="text-align:justify; margin-bottom:0.75rem; margin-top: 0.75rem;">'
-  );
-  styledContent = styledContent.replaceAll(
-    '<p style="text-align:center;">',
-    '<p style="text-align:center; margin-bottom:0.75rem; margin-top: 0.75rem;">'
-  );
-  styledContent = styledContent.replaceAll(
-    '<p style="text-align:right;">',
-    '<p style="text-align:right; margin-bottom:0.75rem; margin-top: 0.75rem;">'
-  );
+  // styledContent = styledContent.replaceAll(
+  //   '<p style="text-align:justify;">',
+  //   '<p style="text-align:justify; margin-bottom:0.75rem; margin-top: 0.75rem;">'
+  // );
+  // styledContent = styledContent.replaceAll(
+  //   '<p style="text-align:center;">',
+  //   '<p style="text-align:center; margin-bottom:0.75rem; margin-top: 0.75rem;">'
+  // );
+  // styledContent = styledContent.replaceAll(
+  //   '<p style="text-align:right;">',
+  //   '<p style="text-align:right; margin-bottom:0.75rem; margin-top: 0.75rem;">'
+  // );
 
-  // <h2> styling
-  styledContent = styledContent.replaceAll(
-    '<h2>',
-    '<h2 style="font-size:1.25rem; font-weight: 600;">'
-  );
+  // // <h2> styling
+  // styledContent = styledContent.replaceAll(
+  //   '<h2>',
+  //   '<h2 style="font-size:1.25rem; font-weight: 600;">'
+  // );
 
-  styledContent = styledContent.replaceAll(
-    '<h2 style="text-align:justify;">',
-    '<h2 style="text-align:justify; font-size:1.25rem; font-weight: 600;">'
-  );
+  // styledContent = styledContent.replaceAll(
+  //   '<h2 style="text-align:justify;">',
+  //   '<h2 style="text-align:justify; font-size:1.25rem; font-weight: 600;">'
+  // );
 
-  styledContent = styledContent.replaceAll(
-    '<h2 style="text-align:center;">',
-    '<h2 style="text-align:center; font-size:1.25rem; font-weight: 600;">'
-  );
+  // styledContent = styledContent.replaceAll(
+  //   '<h2 style="text-align:center;">',
+  //   '<h2 style="text-align:center; font-size:1.25rem; font-weight: 600;">'
+  // );
 
-  styledContent = styledContent.replaceAll(
-    '<h2 style="text-align:right;">',
-    '<h2 style="text-align:right; font-size:1.25rem; font-weight: 600;">'
-  );
+  // styledContent = styledContent.replaceAll(
+  //   '<h2 style="text-align:right;">',
+  //   '<h2 style="text-align:right; font-size:1.25rem; font-weight: 600;">'
+  // );
 
-  // <h3> styling
-  styledContent = styledContent.replaceAll(
-    '<h3>',
-    '<h3 style="font-size:1rem; font-weight: 600;">'
-  );
+  // // <h3> styling
+  // styledContent = styledContent.replaceAll(
+  //   '<h3>',
+  //   '<h3 style="font-size:1rem; font-weight: 600;">'
+  // );
 
-  styledContent = styledContent.replaceAll(
-    '<h3 style="text-align:justify;">',
-    '<h3 style="text-align:justify; font-size:1rem; font-weight: 600;">'
-  );
+  // styledContent = styledContent.replaceAll(
+  //   '<h3 style="text-align:justify;">',
+  //   '<h3 style="text-align:justify; font-size:1rem; font-weight: 600;">'
+  // );
 
-  styledContent = styledContent.replaceAll(
-    '<h3 style="text-align:center;">',
-    '<h3 style="text-align:center; font-size:1rem; font-weight: 600;">'
-  );
+  // styledContent = styledContent.replaceAll(
+  //   '<h3 style="text-align:center;">',
+  //   '<h3 style="text-align:center; font-size:1rem; font-weight: 600;">'
+  // );
 
-  styledContent = styledContent.replaceAll(
-    '<h3 style="text-align:right;">',
-    '<h3 style="text-align:right; font-size:1rem; font-weight: 600;">'
-  );
+  // styledContent = styledContent.replaceAll(
+  //   '<h3 style="text-align:right;">',
+  //   '<h3 style="text-align:right; font-size:1rem; font-weight: 600;">'
+  // );
 
-  // <ul> styling
-  styledContent = styledContent?.replaceAll(
-    '<ul>',
-    '<ul style="list-style: inside;">'
-  );
+  // // <ul> styling
+  // styledContent = styledContent?.replaceAll(
+  //   '<ul>',
+  //   '<ul style="list-style: inside;">'
+  // );
 
 
   return {
