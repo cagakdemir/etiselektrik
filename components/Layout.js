05d6b51141c3f439d67c9ba965d451f579ef90f7
cagakdemir <62038023+cagakdemir@users.noreply.github.com>
2021-06-05 19:48:15 +0300

    tawk.to chat module added

--- components/Layout.js
+++ components/Layout.js
@@ -9,6 +9,19 @@ export default function Layout({ title, keywords, description, children }) {
         <title>{title}</title>
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60bb5650dd60a20abbe4b843/1f7dsib4d';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`,
+          }}
+        />
       </Head>
       <Navbar />
       <div>{children}</div>
